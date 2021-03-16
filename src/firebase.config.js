0c545b5baed3eefd5100c8d1de8288fe9f9112f0
khalid <mdmahfuzbd@gmail.com>
2021-03-16 06:54:42 +0600

    firebase config added

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD77DJFc1OskbEmEJgVSDSFr9oGeyS92q8",
+    authDomain: "ema-john-mkm.firebaseapp.com",
+    projectId: "ema-john-mkm",
+    storageBucket: "ema-john-mkm.appspot.com",
+    messagingSenderId: "577544648077",
+    appId: "1:577544648077:web:498bcab16879b6dec539c4"
+  };
+
+  export default firebaseConfig;
