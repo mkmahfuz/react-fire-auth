b75c7b408ef9bdac01925c9071447a67c9c24df4
khalid <mdmahfuzbd@gmail.com>
2021-03-16 16:12:10 +0600

    mod-42.4 done

--- src/App.js
+++ src/App.js
@@ -13,10 +13,12 @@ function App() {
     isSignedIn: false,
     name: '',
     photo: '',
-    email: ''
+    email: '',
+    password:''
   });
 
   const provider = new firebase.auth.GoogleAuthProvider();
+
   const handleSignIn = () => {
     firebase.auth().signInWithPopup(provider)
       .then(result => {
@@ -35,20 +37,56 @@ function App() {
       })
     // console.log('signed in click')
   }
-  const handleSignOut = ()=>{
+
+  const handleSignOut = () => {
     firebase.auth().signOut()
-    .then(res=>{
-      const signedoutuser = {
-        isSignedIn : false,
-        name: '',
-        photo : '',
-        email:''
-      };
-      setUser(signedoutuser);
-      console.log('sign out successful');
-    }
+      .then(res => {
+        const signedoutuser = {
+          isSignedIn: false,
+          name: '',
+          photo: '',
+          email: ''
+        };
+        setUser(signedoutuser);
+        console.log('sign out successful');
+      }
       )
-    .catch(err=>{console.log(err)})
+      .catch(err => { console.log(err) })
+  }
+
+  const handleSubmit = () => {
+    console.log('own submit clicked')
+
+  }
+  const handleBlur = (evnt) => {
+    let isFormValid = true;
+    if (evnt.target.name === 'email') {
+      const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/; //regex valid email pattern
+      // re.test(evnt.target.value) ? console.log('valid email') : console.log('invalid email') //regex.test(whattoteststring) return true/false
+      // console.log(evnt.target.value)
+      // const isEmailValid = re.test(evnt.target.value);
+      isFormValid = re.test(evnt.target.value);
+      // console.log(isEmailValid)
+    }
+    if (evnt.target.name === 'password') {
+      const isPasswordValid = evnt.target.value.length > 6;
+      //  console.log(isPasswordValid)
+      const re = /\d{1}/;
+      const passwordHasNumber = re.test(evnt.target.value);
+      // console.log(isPasswordValid && passwordHasNumber);
+      isFormValid = isPasswordValid && passwordHasNumber;
+
+      //  ? console.log('password valid') : console.log('password is invalid');
+    }
+if(isFormValid){
+  //[...cart,newcart]
+  const newUserInfo = {...user}; //copy object
+  newUserInfo[evnt.target.name] = evnt.target.value; //set object property
+  setUser(newUserInfo)
+}
+    console.log(evnt.target.name, evnt.target.value);
+    // console.log(event.target.value)
+
   }
 
   return (
@@ -57,7 +95,7 @@ function App() {
         user.isSignedIn ? <button onClick={handleSignOut}>LogOut</button> : <button onClick={handleSignIn}>Sign In</button>
       }
 
-      
+
       {
         user.isSignedIn && <div>
           <p>User Name: {user.name}</p>
@@ -66,6 +104,24 @@ function App() {
         </div>
 
       }
+
+      <div>
+        <h1>Our own authentication system</h1>
+        <div>
+          <p>Name:{user.name}</p>
+          <p>Email:{user.email}</p>
+          <p>Password:{user.password}</p>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input type='text' placeholder='name' name='name' onBlur={handleBlur}/><br/>
+          <input type='text' onBlur={handleBlur} name='email' placeholder='email' required /><br />
+          <input type='password' onBlur={handleBlur} name='password' placeholder='password' required /><br />
+          <input type='submit' value='Submit' />
+
+          {/* <button type='submit'>Submit</button> */}
+        </form>
+      </div>
+
 
     </div>
   );
