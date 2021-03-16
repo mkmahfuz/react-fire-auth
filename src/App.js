666d4aa0a55bb63dfcb72bfd798da641697d5334
khalid <mdmahfuzbd@gmail.com>
2021-03-16 09:16:25 +0600

    module 41 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    photo: '',
+    email: ''
+  });
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSignIn = () => {
+    firebase.auth().signInWithPopup(provider)
+      .then(result => {
+        const { displayName, photoURL, email } = result.user;
+        const signedInUser = {
+          isSignedIn: true,
+          name: displayName,
+          photo: photoURL,
+          email: email
+        }
+        setUser(signedInUser);
+        console.log(displayName, photoURL, email)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    // console.log('signed in click')
+  }
+  const handleSignOut = ()=>{
+    firebase.auth().signOut()
+    .then(res=>{
+      const signedoutuser = {
+        isSignedIn : false,
+        name: '',
+        photo : '',
+        email:''
+      };
+      setUser(signedoutuser);
+      console.log('sign out successful');
+    }
+      )
+    .catch(err=>{console.log(err)})
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        user.isSignedIn ? <button onClick={handleSignOut}>LogOut</button> : <button onClick={handleSignIn}>Sign In</button>
+      }
+
+      
+      {
+        user.isSignedIn && <div>
+          <p>User Name: {user.name}</p>
+          <p>Photo: <img src={user.photo} alt="" /></p>
+          <p>Email: {user.email}</p>
+        </div>
+
+      }
+
     </div>
   );
 }
